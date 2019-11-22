a3fe60cff7b5498b76a0ebdb44ce732b51fc4c87
“marcusvrnleal” <marcusvrnleal84@gmail.com>
2019-11-22 16:21:13 -0400

    "2.1"

--- 3. Listas de Exercícios/Atividade Imediata 7/Lab 03/Uri1040.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-public class Uri1040{
-    public static void main(String []args){
-        Scanner teclado = new Scanner(System.in);
-
-    }
-}
--- 5. Prova/Gabarito de Prog1/1. Loop, Sequencial e Variaveis/OperadoresUnarios2.java
+++
@@ -1,9 +0,0 @@
-public class OperadoresUnarios2{
-	public static void main (String [] args){
-		int n = 3;
-		int m = 4;
-		int result = n * (m++);
-
-		System.out.println(result);
-	}
-}
