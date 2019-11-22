ea3c664e3a5e3ff1ea81a20a6dcdd7f308e0b263
“marcusvrnleal” <marcusvrnleal84@gmail.com>
2019-11-22 16:16:28 -0400

    1.2

--- 4. Trabalho Prático/Trabalho Prático I/Parte 1/Uri1016.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Uri1016{
-	public static void main(String[] args){
-		Scanner teclado = new Scanner(System.in);
-		int distancia = teclado.nextInt();
-		System.out.println((distancia * 2) + " minutos");
-		teclado.close();
-	}
-}
